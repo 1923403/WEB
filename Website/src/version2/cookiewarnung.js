1d2c8842e7eeab4a7f3963f4671e80249f82a30d
Tobias-Be <tobi.benrath@gmail.com>
2020-12-10 10:37:13 +0100

    javascript in extra datei

---
+++ Website/src/version2/cookiewarnung.js
@@ -0,0 +1,9 @@
+
+function cookieWarning() {
+    var x = document.getElementById("cookie");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
